c9e850defa9e3a56ae9b784274ecc4106619acae
Karthigeyan Vijayakumar <karthigeyan.v@gmail.com>
2021-07-24 10:59:59 -0700

    Project 1 - WeSplit

---
+++ Project1 - WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Karthigeyan Vijayakumar on 7/22/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
