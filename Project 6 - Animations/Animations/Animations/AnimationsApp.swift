f0b11623e1a944bf097daee9279b063569cbb1d6
Karthigeyan Vijayakumar <karthigeyan.v@gmail.com>
2021-08-11 06:44:16 -0700

    Project 6 - Animations - Tech Project

---
+++ Project 6 - Animations/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Karthigeyan Vijayakumar on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
